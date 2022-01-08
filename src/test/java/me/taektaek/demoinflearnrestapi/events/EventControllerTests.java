40e24d42401413f94ab5d93a520d72c5e949312c
ITHwang <intaek428@gmail.com>
2022-01-08 11:25:17 +0900

    POST /api/events return 201

--- src/test/java/me/taektaek/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/taektaek/demoinflearnrestapi/events/EventControllerTests.java
@@ -1,13 +1,20 @@
 package me.taektaek.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -16,11 +23,38 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    //MockMvc는 웹과 관련된 빈들만 목으로 등록하기 때문에 리포지토리 빈은 따로 생성해줘야 함
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEventDateTime(LocalDateTime.of(2022, 1, 7, 19, 36, 50))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 1, 8, 19, 36, 50))
+                .beginEventDateTime(LocalDateTime.of(2022, 11, 9, 20, 11, 23))
+                .endEventDateTime(LocalDateTime.of(2022, 11, 23, 12, 32, 43))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남의 D2 스타스업 팩토리")
+                .build();
+
+        //mockbean은 save()해도 null을 반환함
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
