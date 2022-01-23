62c8cf9711ebc40f537fc44b0fcac404adeb1c4e
ITHwang <intaek428@gmail.com>
2022-01-23 23:50:42 +0900

    spring.secuirty.oauth가 deprecated되어 따로 공부해야 함.

---
+++ src/main/java/me/taektaek/demoinflearnrestapi/configs/AuthServerConfig.java
@@ -0,0 +1,11 @@
+package me.taektaek.demoinflearnrestapi.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+
+@Configuration
+public class AuthServerConfig extends OAuth2AuthorizationServerConfiguration {
+
+
+
+}
