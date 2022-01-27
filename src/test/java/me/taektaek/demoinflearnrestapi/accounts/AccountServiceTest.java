15220b7b181e6204d2dd5b4ef22fc648e2469803
ITHwang <intaek428@gmail.com>
2022-01-27 23:17:27 +0900

    예외 테스트 추가

--- src/test/java/me/taektaek/demoinflearnrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/taektaek/demoinflearnrestapi/accounts/AccountServiceTest.java
@@ -1,15 +1,18 @@
 package me.taektaek.demoinflearnrestapi.accounts;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -22,6 +25,7 @@ class AccountServiceTest {
     AccountRepository accountRepository;
 
     @Test
+    @DisplayName("인증 테스트")
     public void findByUsername() {
         //Given
         String password = "intaek";
@@ -39,7 +43,14 @@ class AccountServiceTest {
 
         //Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
 
+    @Test
+    @DisplayName("해당 아이디가 없을 경우 오류가 나는 테스트")
+    public void findByUsernameFail() {
+        String username = "random@email.com";
+        Exception exception = assertThrows(UsernameNotFoundException.class, () -> accountService.loadUserByUsername(username));
+        assertThat(exception).hasMessageContaining(username);
     }
 
 }
