cc857044599f24dc463c63f417ac828b1d58a5c3
ITHwang <intaek428@gmail.com>
2022-01-27 21:50:23 +0900

    Revert "spring security: 인증 관련 예외 테스트 추가"
    This reverts commit 7d2ae23d753c3eb369046afd73841a1708999159.

--- src/test/java/me/taektaek/demoinflearnrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/taektaek/demoinflearnrestapi/accounts/AccountServiceTest.java
@@ -1,20 +1,15 @@
 package me.taektaek.demoinflearnrestapi.accounts;
 
-import lombok.ToString;
-import org.junit.Rule;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,7 +22,6 @@ class AccountServiceTest {
     AccountRepository accountRepository;
 
     @Test
-    @DisplayName("인증 테스트")
     public void findByUsername() {
         //Given
         String password = "intaek";
@@ -45,16 +39,7 @@ class AccountServiceTest {
 
         //Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+
     }
-
-    @Test
-    @DisplayName("해당 아이디가 없을 경우 오류가 나는 예외 테스트")
-    public void findByUsernameFail() {
-        String username = "random@email.com";
-        Exception exception = assertThrows(UsernameNotFoundException.class, () -> accountService.loadUserByUsername(username));
-        assertThat(exception).hasMessageContaining(username);
-    }
-
-
 
 }
